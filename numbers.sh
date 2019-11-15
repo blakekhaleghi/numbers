ae45fd2b5b63e63f47614bbd4538a86ee7284447
blakekhaleghi <56717554+blakekhaleghi@users.noreply.github.com>
2019-11-15 11:23:22 -0800

    Create numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers.sh
+# Blake Khaleghi
+echo "Enter a positive number please"
+read NUM
+N=1
+while [ "$N" -le "$NUM" ]
+do
+         if [ $((N%2)) -eq 0 ]
+         then
+                 echo "$N EVEN"
+                 N=$((N+1))
+         else 
+                 echo "$N Odd"
+                 N=$((N+1))
+         fi
+done 
